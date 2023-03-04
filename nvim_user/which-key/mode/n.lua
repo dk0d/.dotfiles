1d61577a04fad9f00aa22654aaf6a3537eb4aadb
Daniel Capecci <5144601-dk0d@users.noreply.gitlab.com>
2023-03-04 17:18:31 -0500

    move to lazyvim

--- nvim_user/which-key/mode/n.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- second key is the prefix, <leader> prefixes
-	['<leader>'] = {
-		-- third key is the key to bring up next level and its displayed
-		-- group name in which-key top level menu
-		['b'] = { name = 'Buffer' },
-	},
-}
