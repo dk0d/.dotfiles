7a1cc67312ca14966faf59c64d2c57429ceafc47
Daniel Capecci <5144601-dk0d@users.noreply.gitlab.com>
2023-03-04 17:50:13 -0500

    lazy updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,30 +1,44 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local function map(mode, lhs, rhs, opts)
-    vim.keymap.set(mode, lhs, rhs, opts)
-end
 
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+    local keys = require("lazy.core.handler").handlers.keys
+    ---@cast keys LazyKeysHandler
+    -- do not create the keymap if a lazy keys handler exists
+    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+        opts = opts or {}
+        opts.silent = opts.silent ~= false
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
+end
 map('i', 'jk', "<esc>", { desc = "Leave insert mode", noremap = true, silent = true })
 
 -- Line ops
-map('v', 'J', "<cmd> m '>+1<cr>gv=gv", { desc = "Move line down" })
-map('v', 'K', "<cmd> m '<-2<cr>gv=gv", { desc = "Move line up" })
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Telescope
-map('n', 'sx', require('telescope.builtin').resume, { desc = "Resume previous picker" })
-map('n', 'sf', require('telescope.builtin').find_files, { desc = "Find Files" })
+map('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = "Search Keymaps" })
+map('n', '<leader>sx', require('telescope.builtin').resume, { desc = "Resume previous picker" })
+map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = "Find Files" })
 map('n', "<leader>fb", require("telescope.builtin").buffers, { desc = "Search buffers" })
-map('n', "<leader>fh", require("telescope.builtin").help_tags, { desc = "Search help" })
-map('n', "<leader>fm", require("telescope.builtin").marks, { desc = "Search marks" })
-map('n', "<leader>fo", require("telescope.builtin").oldfiles, { desc = "Search history" })
-map('n', "<leader>fc", require("telescope.builtin").grep_string,
+map('n', "<leader>sp", require("telescope.builtin").help_tags, { desc = "Search help" })
+map('n', "<leader>sh", require("telescope.builtin").oldfiles, { desc = "Search history" })
+map('n', "<leader>sc", require("telescope.builtin").grep_string,
     { desc = "Search for word under cursor" })
-map('n', "<leader>sm", function() require("telescope.builtin").man_pages() end, { desc = "Search man" })
-map('n', "<leader>sr", function() require("telescope.builtin").registers() end, { desc = "Search registers" })
+map('n', "<leader>sm",  require("telescope.builtin").marks, { desc = "Search marks" })
+map('n', "<leader>sM",  require("telescope.builtin").man_pages, { desc = "Search man" })
+map('n', "<leader>sr",  require("telescope.builtin").registers, { desc = "Search registers" })
 -- Git
-map('n', 'gt', require('telescope.builtin').git_status, { desc = "Git Status" })
-map('n', 'gb', require('telescope.builtin').git_branches, { desc = "Git Branches" })
+map('n', '<leader>gt', require('telescope.builtin').git_status, { desc = "Git Status" })
+map('n', '<leader>gb', require('telescope.builtin').git_branches, { desc = "Git Branches" })
 
 -- DAP
 map('n', '<leader>b', "<cmd> lua require'dap'.toggle_breakpoints()<cr>", { desc = 'Toggle Breakpoints' })
@@ -32,8 +46,9 @@ map('n', '<leader>b', "<cmd> lua require'dap'.toggle_breakpoints()<cr>", { desc 
 -- Format
 map('n', '<leader>fd',
     function()
-        vim.lsp.buf.format(astronvim.lsp.format_opts)
+        vim.lsp.buf.format()
     end,
     { desc = 'Format Code' }
 )
+
 
---
+++ nvim_bak/lua/plugins/dap.lua
@@ -0,0 +1,47 @@
+local getpython = require('user.utils').getpython
+require('dap.ext.vscode').load_launchjs()
+return {
+  adapters = {
+    python = {
+      type = 'executable',
+      command = getpython(),
+      args = { '-m', 'debugpy.adapter' },
+    },
+    rust = {
+      type = 'executable',
+      command = 'cargo run',
+      args = { '--color', 'always' },
+    },
+  },
+  configurations = {
+    rust = {
+      {
+        type = 'lldb',
+        request = 'launch',
+        name = 'Run File',
+        cwd = '${workspaceFolder}',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        stopOnEntry = false,
+        args = {},
+        runInTerminal = false,
+      },
+      {
+        type = 'lldb',
+        request = 'launch',
+        name = 'Run Project',
+        program = '${workspaceFolder}/src/main.rs',
+      },
+    },
+    -- 	python = {
+    -- 		{
+    -- 			type = "python",
+    -- 			request = "launch",
+    -- 			name = "Launch File",
+    -- 			program = "${file}",
+    -- 			pythonPath = getpython(),
+    -- 		},
+    -- 	},
+  },
+}
